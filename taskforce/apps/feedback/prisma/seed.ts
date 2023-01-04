2a4c53dd33ee29ababc18554ba61417757cfa1bd
Vitalii Iashchuk <iashchuk@hotmail.com>
2023-01-04 22:58:51 +0300

    M4T1: update config prisma client

--- taskforce/apps/feedback/prisma/seed.ts
+++ taskforce/apps/feedback/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client-feedback';
 
 const prisma = new PrismaClient();
 
--- taskforce/apps/tasks/prisma/seed.ts
+++ taskforce/apps/tasks/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client-tasks';
 
 const prisma = new PrismaClient();
 
